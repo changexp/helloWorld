fc4bde20845dc0ec289e131cf5084ce6c1dd5a62
changexp <cxpydjr@gmail.com>
2019-04-02 16:12:11 +0800

    test

--- src/com/cxp/HelloWorld.java
+++ src/com/cxp/HelloWorld.java
@@ -1,7 +1,9 @@
 package com.cxp;
-
+import java.util.*;
 public class HelloWorld {
     public void hello(){
         System.out.println("hello,world");
+        List<Integer> list=new ArrayList<>();
+        list.add(1);
     }
 }
